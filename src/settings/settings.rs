166185abe56d00405f233ea828698d36c751a17a
Fre <hello@hi.wow>
2026-07-25 14:58:35 +0200

    default settings

--- src/settings/settings.rs
+++ src/settings/settings.rs
@@ -1,17 +1,24 @@
-struct Settings {
+pub(crate) struct Settings {
     n_players: u8,
 
     dice_thrown: u32,
-    dice_throw_type: DiceThrow,
+    dice_throw_type: DiceThrowType,
 }
 
 
 impl Default for Settings {
+    fn default() -> Settings {
+        Settings {
+            n_players: 8,
 
+            dice_thrown: 6,
+            dice_throw_type: AUTOMATIC_FOR_BOTS
+        }
+    }
 }
 
 
-enum DiceThrow {
+enum DiceThrowType {
     AUTOMATIC,
     AUTOMATIC_FOR_BOTS,
     MANUAL
